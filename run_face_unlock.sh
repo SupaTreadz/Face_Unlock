2c2c39d959fd5622a5fec68c7893ed9e04aacda8
supatreadz <suptreadz@gmail.com>
2020-06-03 19:43:20 -0500

    Adding files

---
+++ run_face_unlock.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 recognize.py --detector face_detection_model --embedding-model openface_nn4.small2.v1.t7 --recognizer output/recognizer.pickle --le output/le.pickle --image images/Image.jpg
