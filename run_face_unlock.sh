77cfb043e8c2c27ea61916b07fc2de3bf0ef1660
SupaTreadz <supatreadz@gmail.com>
2020-08-15 00:04:48 -0500

    Update run_face_unlock.sh

--- run_face_unlock.sh
+++ run_face_unlock.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 recognize.py --detector face_detection_model --embedding-model openface_nn4.small2.v1.t7 --recognizer output/recognizer.pickle --le output/le.pickle --image images/Image.jpg
+python3 /home/pi/Face_Unlock/recognize.py --detector /home/pi/Face_Unlock/face_detection_model --embedding-model /home/pi/Face_Unlock/openface_nn4.small2.v1.t7 --recognizer /home/pi/Face_Unlock/output/recognizer.pickle --le /home/pi/Face_Unlock/output/le.pickle --image /home/pi/Face_Unlock/images/Image.jpg
